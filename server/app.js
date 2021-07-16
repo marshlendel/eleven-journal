b9474d83e0375b8e56f2a90c90cec0dd34fe507a
Marshall Brown <marshallendel@gmail.com>
2021-07-16 17:07:55 -0400

    add test endpoint

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const Express = require('express')
 const app = Express()
 
+app.use("/test", (req, res) => {
+    res.send("This is a message from the test endpoint on the server!")
+})
 app.listen(3000, () => {
     console.log("[Server]: App is listening on port 3000")
 })
